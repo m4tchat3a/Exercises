f02c5c0823ba72475909273819ea55b7370c7be4
matcha <90914372+m4tchat3a@users.noreply.github.com>
2023-03-28 22:56:44 -0300

    Create areas.h

---
+++ c/areas.h
@@ -0,0 +1,7 @@
+//areas.h
+
+#define       Pi            3.14
+#define       ACirculo      (Pi*(r*r))
+#define       AQuadrado     ((x)*(x))
+#define       ARetangulo    (x*y)
+#define       ATriangulo    (x*y/2)
