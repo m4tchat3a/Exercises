b7227782ff58b712b1b29b14d375a66e41c38fe9
matcha <90914372+m4tchat3a@users.noreply.github.com>
2023-03-29 11:01:40 -0300

    Create 3L1D.cpp

---
+++ c/3L1D.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+
+printf ("\nNome do grupo: 3L1D");
+printf ("\nTurma: 1o Internet E");
+printf ("\nNumero: 4 Nome: Daniel Couto Cardoso");
+printf ("\nNumero: 14 Nome: Lais de Souza Freitas");
+printf ("\nNumero: 15 Nome: Leandro dos Santos Bido");
+printf ("\nNumero: 37 Nome: Luanda Catarina Aureliano Moreira");
+getch();
+}
